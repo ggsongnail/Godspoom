b7e31fe3d6d96b833ba624280f90b5256034e485
ggsongnail <343851557@qq.com>
2016-09-19 17:49:17 +0800

    事务回滚，@Version，保存测试

--- src/test/java/com/spoom/service/UserServiceJunitTest.java
+++ src/test/java/com/spoom/service/UserServiceJunitTest.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spoom.entity.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-jpa.xml")
@@ -15,5 +19,24 @@ public class UserServiceJunitTest {
 	@Test
 	public void testFindAll(){
 		System.out.println(userService.findAll().size());
+	}
+	
+	
+	public void testSave(){
+		User user = userService.findById(2);
+		user.setName("Jony");
+		userService.save(user);
+	}
+	
+	
+	public void testInsert(){
+		User user = new User();
+		user.setName("Jony");
+		user.setPhone("13414985655");
+		userService.save(user);
+	}
+	
+	public void testTransactionRollBack(){
+		userService.testTransactionRollBack(1);
 	}
 }
